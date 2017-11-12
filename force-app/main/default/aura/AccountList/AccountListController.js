d0ee19367935d78a5d07512443ed042d42a4ea39
Robby <robert.x.williams@oracle.com>
2017-11-11 22:43:43 -0600

    Added components to display each account in app
     - Module : App Development with Salesforce DX
     - Task : Build the Geolocation App Using Salesforce CLI
     - Includes : New component bundles and styles

---
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -0,0 +1,16 @@
+({
+    doInit : function(component, event) {
+        var action = component.get("c.findAll");
+        action.setCallback(
+            this,
+            function(a) {
+                component.set(
+                    "v.accounts",
+                    a.getReturnValue()
+                );
+            }
+        );
+        $A.enqueueAction(action);
+    }
+})
+    
